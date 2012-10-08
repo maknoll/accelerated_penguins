ec63693410c7451ee99edacd03b3897d3c300223
Martin Knoll <martinknoll@me.com>
2012-10-08 15:21:46 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AcceleratedPenguins::Application.config.secret_token = '350e00991ae50b760d7909ba8c96f84cfcb23a523d172059268b13b30b0ae1a40f727e1141ac1510943bb1d51b44f7b28d11eb49431f76500ac61a6f0ef6ca2b'
