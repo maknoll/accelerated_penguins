03a86e3802413608c1f7f6163762e8c206168b5f
Martin Knoll <martinknoll@me.com>
2012-10-13 01:18:28 +0200

    action caching

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ end
 
 group :production do
   gem 'pg'
+  gem 'dalli'
 end
 
 gem 'thin'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,22 @@
 class PostsController < InheritedResources::Base
   before_filter :authenticate, except: [:index, :show]
+  caches_action :index, :show
 
   def create
+    expire_action :action => :index
     @post = Post.new(params[:post])
     @post.author = current_user
     create!
   end
+
+  def update
+    expire_action :action => [:index, :show]
+    update!
+  end
+
+  def destroy
+    expire_action :action => [:index, :show]
+    destroy!
+  end
+
 end
